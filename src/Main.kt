535dd484d1f40e2a153062edc897a9804c25c417
Komang Yuda Saputra <yudasaputra082@gmail.com>
2024-03-01 23:16:42 +0700

    add calculate triangle feature

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,17 @@
+class Triangle(val height : Float, val base:Float ){
+
+    fun calculateArea(){
+        println("Luas Segitiga ${(base * height)/2}")
+    }
+    fun calculatePerimeter(){
+        println("Keliling Segitiga ${3*base}")
+    }
+}
+
+
 fun main() {
-    println("Hello World!")
+    val triangle = Triangle(4.0F, 3.0F)
+    println(triangle.calculateArea())
+    println(triangle.calculatePerimeter())
+
 }
